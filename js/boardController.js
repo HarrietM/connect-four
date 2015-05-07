59ff443686002cb865fef363ede1ef438c2de069
HarrietM <hsemckinnon@gmail.com>
2015-05-07 20:11:03 +1200

    Horizantal win-state almost functional

--- js/boardController.js
+++ js/boardController.js
@@ -6,7 +6,6 @@ boardController.prototype.run = function(){
   var board = new Board(this.selector);
   var game = new Game();
   var player = new Player();
-  console.log(this.player)
   // var player1 = new Player(this.selector, 1);
   // var player2 = new Player(this.selector, 2);
   board.load();
--- js/game.js
+++ js/game.js
@@ -23,25 +23,61 @@ Game.prototype.placePlayerPiece = function(r, player){
   var r = r.selector
   for (var i = 5; i >= 0; i--){
     if (board[r][i].status == 0){
-      board[r][i].status = 1;
-      console.log(player.num)
+      board[r][i].status = player.num;
       $("#r"+r+"_c"+i).addClass("player"+player.num)
       if (player.num == 1){
         player.num = 2
+        this.win(r,i)
       }else {
         player.num = 1
+        this.win(r,i)
       }
       break;
     }
   }
 }
 
+Game.prototype.win = function(r,c){
+  if(this.horizontalWin(r,c)){
+    return true
+    console.log("win!")
+  }
+}
+
+Game.prototype.horizontalWin = function(r,c){
+ var winState = [[-1,2,3],[1,2,3],[-3,-2,-1],[-2,-1,1]]
+ var win = [];
+ var board = this.board;
+ var player = board[r][c].status;
+
+ winState.forEach(function(state){
+  var tilesStatus = []
+  state.forEach(function(coordinate){
+    if(board[r][c+coordinate].status == player){
+      tilesStatus.push(1)
+    }
+  });
+  if(tilesStatus.length == 3){
+    win.push("win")
+    console.log(win)
+  }
+ });
+
+ console.log(win)
+
+ if(win.includes("win")){
+   console.log("yay")
+   return true
+ }
+
+}
+
 function Tile(){
   this.status = 0;
 }
 
-test = new Game();
-test.createBoard();
-test.placePlayerPiece(3);
+// test = new Game();
+// test.createBoard();
+// test.placePlayerPiece(3);
 // test.placePlayerPiece(3, 2);
 // console.log(test.board)
