013a97ebb35faae9d56984f3297ddd4ead06b7df
HarrietM <hsemckinnon@gmail.com>
2015-05-08 14:36:42 +1200

    vert + horizontal wins working. Marginal refactor

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 function Game(){
  this.board = [];
+ this.WIN_STATES = [[-1,1,2],[1,2,3],[-3,-2,-1],[-2,-1,1]];
+ this.won = false;
 }
 
 Game.prototype.createBoard = function(){
@@ -38,45 +40,115 @@ Game.prototype.placePlayerPiece = function(r, player){
 }
 
 Game.prototype.win = function(r,c){
-  if(this.horizontalWin(r,c)){
-    console.log("win!")
+  var board = this.board;
+  if(this.horizontalOrVerticalWin(r,c)){
+    var winner = board[r][c].status;
+    alert("Player "+winner+" is the winner!")
     return true
   }
 }
 
-Game.prototype.horizontalWin = function(r,c){
- var winState = [[-1,2,3],[1,2,3],[-3,-2,-1],[-2,-1,1]]
- var win = [];
- var won = false;
- var board = this.board;
- var bounds = this.inBounds;
- var player = board[r][c].status;
+Game.prototype.horizontalOrVerticalWin = function(r,c){
+  var win = [];
+  var that = this;
+  var player = that.board[r][c].status;
 
- winState.forEach(function(state){
-  var tilesStatus = [];
-  state.forEach(function(coordinate){
-    if(bounds(r,c+coordinate)){
-      if(board[r][c+coordinate].status == player){
+  this.WIN_STATES.forEach(function(state){
+   var tilesStatus = [];
+   state.forEach(function(coordinate){
+      if(that.horizontalWin(r,c,coordinate, player) || that.verticalWin(r,c,coordinate, player) ){
         tilesStatus.push(1);
       }
-    }
+    });
+   if(tilesStatus.length == 3){
+     win.push("win");
+   }
   });
-  if(tilesStatus.length == 3){
-    win.push("win");
+
+  if(win.indexOf("win") == 0){
+    that.won = true;
   }
- });
 
- if(win.indexOf("win") == 0){
-  console.log("in here")
-   won = true;
- }
-
- return won;
+  return that.won;
 }
 
+Game.prototype.horizontalWin = function(r,c,coordinate,player){
+  var that = this;
+  var status = false;
+  if(that.inBounds(r,Number(c)+Number(coordinate))){
+    if(that.board[Number(r)][Number(c)+Number(coordinate)].status == player){
+      status = true;
+    }
+  }
+  return status;
+}
+
+Game.prototype.verticalWin = function(r,c,coordinate,player){
+  var that = this;
+  var status = false;
+  if(that.inBounds(Number(r)+Number(coordinate),Number(c))){
+    if(that.board[Number(r)+Number(coordinate)][c].status == player){
+      status = true;
+    }
+  }
+  return status;
+}
+
+// Game.prototype.horizontalWin = function(r,c){
+//  var win = [];
+//  var that = this;
+//  var player = that.board[r][c].status;
+
+//  this.WIN_STATES.forEach(function(state){
+//   var tilesStatus = [];
+//   state.forEach(function(coordinate){
+//     if(that.inBounds(r,c+coordinate)){
+//       if(that.board[r][c+coordinate].status == player){
+//         tilesStatus.push(1);
+//       }
+//     }
+//   });
+//   if(tilesStatus.length == 3){
+//     win.push("win");
+//   }
+//  });
+
+//  if(win.indexOf("win") == 0){
+//    that.won = true;
+//  }
+
+//  return that.won;
+// }
+
+// Game.prototype.verticalWin = function(r,c){
+//  var win = [];
+//  var that = this;
+//  var player = that.board[r][c].status;
+
+//  this.WIN_STATES.forEach(function(state){
+//   var tilesStatus = [];
+//   state.forEach(function(coordinate){
+//     if(that.inBounds(Number(r)+Number(coordinate),Number(c))){
+//       if(that.board[Number(r)+Number(coordinate)][c].status == player){
+//         tilesStatus.push(1);
+//       }
+//     }
+//   });
+//   if(tilesStatus.length == 3){
+//     win.push("win");
+//   }
+//  });
+
+//  if(win.indexOf("win") == 0){
+//    that.won = true;
+//  }
+
+//  return that.won;
+// }
+
 Game.prototype.inBounds = function(r, c) {
- if(r > 0 && r < 6){
-  if(c > 0 && c < 7){
+ if(r >= 0 && r < 7){
+  if(c >= 0 && c < 6){
     return true
   }
  }
