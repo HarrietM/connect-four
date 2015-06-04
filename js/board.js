93ec8e516790a473543c453742b3a1bd005bb588
HarrietM <hsemckinnon@gmail.com>
2015-06-04 20:17:01 +1200

    Change prototyping of board

--- js/board.js
+++ js/board.js
@@ -2,34 +2,37 @@ function Board(selector){
   this.selector = selector;
 }
 
-Board.prototype.load = function(){
+Board.prototype = {
 
-  $(this.selector).append("<div class='board'></div>");
+  load: function(){
+    $(this.selector).append("<div class='board'></div>");
 
-  for (var i = 0; i < 7; i++){
-    var columnButton = "<div class='column-button' id="+i+"></div>";
-    $(".board").append(columnButton);
-  }
-
-  for (var c = 0; c < 6; c++){
-    for (var r = 0; r < 7; r++){
-      var tile = "<div class='tile' id=r"+r+"_c"+c+"></div>"
-      $(".board").append(tile);
+    for (var i = 0; i < 7; i++){
+      var columnButton = "<div class='column-button' id="+i+"></div>";
+      $(".board").append(columnButton);
     }
-  }
-}
 
-Board.prototype.selectColumn = function(game, player, tileID) {
-  $(this.selector).on("click", ".column-button", function(e){
-    e.preventDefault();
-    game.placePlayerPiece($(e.target.id), player, tileID);
-  });
-}
+    for (var c = 0; c < 6; c++){
+      for (var r = 0; r < 7; r++){
+        var tile = "<div class='tile' id=r"+r+"_c"+c+"></div>"
+        $(".board").append(tile);
+      }
+    }
+  },
 
-Board.prototype.tileID = function(r, i, num){
+  selectColumn: function(game, player, tileID) {
+    $(this.selector).on("click", ".column-button", function(e){
+      e.preventDefault();
+      game.placePlayerPiece($(e.target.id), player, tileID);
+    });
+  },
+
+  tileID: function(r, i, num){
     $("#r"+r+"_c"+i).addClass("player"+num)
-}
+  },
 
-Board.prototype.win = function(winner){
-  swal("Player "+winner, "You are the winner!", "success")
+  win: function(winner){
+    swal("Player "+winner, "You are the winner!", "success")
+  }
+
 }
